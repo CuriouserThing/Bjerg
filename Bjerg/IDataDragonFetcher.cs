c486f846c7324c1da1a30005383ec4d05ad42e5f
TheMostCuriousThing <adelaidekwalter@gmail.com>
2020-10-21 22:46:21 -0400

    Add XML documentation.

--- Bjerg/IDataDragonFetcher.cs
+++ Bjerg/IDataDragonFetcher.cs
@@ -7,8 +7,21 @@ namespace Bjerg
 {
     public interface IDataDragonFetcher : IDisposable
     {
+        /// <summary>
+        /// Fetch a Data Dragon globals DTO in a thread-safe manner.
+        /// </summary>
+        /// <param name="locale">The globals locale.</param>
+        /// <param name="version">The globals version.</param>
+        /// <returns>The globals DTO. If non-null, the function succeeded. If null, the function failed.</returns>
         Task<DdGlobals?> FetchGlobals(Locale locale, Version version);
 
+        /// <summary>
+        /// Fetch an array of Data Dragon card DTOs for a particular set in a thread-safe manner.
+        /// </summary>
+        /// <param name="locale">The cards' locale.</param>
+        /// <param name="version">The cards' version.</param>
+        /// <param name="setNumber">The cards' set.</param>
+        /// <returns>The card DTOs. If non-null, the function succeeded. If null, the function failed.</returns>
         Task<IReadOnlyList<DdCard>?> FetchSetCards(Locale locale, Version version, int setNumber);
     }
 }
